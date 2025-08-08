0f1bf6e219d0f8f4c82c0c71a48b811423153a62
James Martin <james@martn.io>
2025-08-08 14:58:39 -0400

    add async fn tool executor

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,25 +1,25 @@
 //! Prelude module for the Commune project.
 //!
 //! This module re-exports commonly used items from various parts of the crate,
-//! as well as from external dependencies. Using `use commune::prelude::*;`
+//! as well as from external dependencies. Using `use mcp_commune::prelude::*;`
 //! will import these items into scope, making it easier to work with the Commune library.
 
 // Re-export items from the crate's modules
-pub use crate::{client::*, error::*, peer::*, server::*};
+pub use crate::{client::*, error::*, peer::*, server::*, tool::{async_fn_executor, sync_fn_executor}};
 
 // Re-export items from external dependencies
 pub use mcp_sdk_rs::types::LoggingLevel;
 
 /// The prelude module.
 ///
-/// This module is intended to be glob-imported (`use commune::prelude::*;`) to bring
+/// This module is intended to be glob-imported (`use mcp_commune::prelude::*;`) to bring
 /// commonly used items into scope. It includes re-exports from various parts of the
 /// Commune crate as well as selected items from external dependencies.
 ///
 /// # Example
 ///
 /// ```
-/// use commune::prelude::*;
+/// use mcp_commune::prelude::*;
 ///
 /// // Now you can use items from client, error, peer, and server modules
 /// // as well as LoggingLevel from mcp_sdk_rs without additional imports.
@@ -34,5 +34,5 @@ pub use mcp_sdk_rs::types::LoggingLevel;
 /// - `LoggingLevel` from `mcp_sdk_rs::types`
 ///
 /// By using this prelude, you can reduce the number of explicit imports needed
-/// when working with the Commune library, leading to cleaner and more concise c
+/// when working with the Commune library, leading to cleaner and more concise code.
 pub use super::*;
